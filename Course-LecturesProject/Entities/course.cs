8cefc7e16cb1b980bbd25e3293c0be10f5112b0b
David Matoušek <MatousekDavid@sssvt.cz>
2026-01-19 09:21:38 +0100

    Creation of a project and adding Entities. Database is connected

---
+++ Course-LecturesProject/Entities/course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("courseTable")]
+public class Courses
+{
+
+	[Key]
+	[Column("courseID")]
+	public int CourseID { get; set; }
+	[Column("courseName")]
+	public string CourseName { get; set; }
+	[Column("description")]
+	public string Description { get; set; }
+	[Column("capacity")]
+	public int Capacity { get; set; }
+	[Column("startDate")]
+	public DateTime StartDate { get; set; }
+	[Column("courseLeader")]
+	public int CourseLeader { get; set; }
+
+    public Courses(int courseID, string courseName, string description, int capacity, DateTime startDate, int courseLeader )
+	{
+		CourseID = courseID;
+		CourseName = courseName;
+		Description = description;
+		Capacity = capacity;
+		StartDate = startDate;
+		CourseLeader = courseLeader;
+    }
+}
