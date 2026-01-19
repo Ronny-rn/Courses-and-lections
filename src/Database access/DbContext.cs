59bf38f47677c98414c071a7cb89855fb02b497d
David Matoušek <MatousekDavid@sssvt.cz>
2026-01-19 07:25:58 +0100

    Created Entities folder and Database access

---
+++ src/Database access/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+	
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=4c1_matousekdavid_db2;user=matousekdavid;password=123456");
+    }
+
+}
