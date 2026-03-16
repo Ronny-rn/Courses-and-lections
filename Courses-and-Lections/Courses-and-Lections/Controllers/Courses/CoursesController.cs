581bdcd9d6fc462c771e5eb76fc39798cdd28831
David Matoušek <MatousekDavid@sssvt.cz>
2026-03-16 06:22:03 +0100

    oprava

--- Courses-and-Lections/Courses-and-Lections/Controllers/Courses/CoursesController.cs
+++ Courses-and-Lections/Courses-and-Lections/Controllers/Courses/CoursesController.cs
@@ -113,7 +113,7 @@ public class CoursesController : ControllerBase
             Message = "Course successfully updated"
         });
     }
-    [Authorize]  
+    [Authorize]
     [HttpDelete("{courseId}")]
     public async Task<ActionResult<DeleteCourseResponse>> DeleteCourse(int courseId)
     {
@@ -123,7 +123,13 @@ public class CoursesController : ControllerBase
         if (course is null)
             return NotFound("Course not found");
 
+        // Remove related OrderItems first
+        var relatedOrderItems = _context.OrderItems
+            .Where(x => x.CourseId == courseId);
+
+        _context.OrderItems.RemoveRange(relatedOrderItems);
         _context.Courses.Remove(course);
+
         await _context.SaveChangesAsync();
 
         return Ok(new DeleteCourseResponse
